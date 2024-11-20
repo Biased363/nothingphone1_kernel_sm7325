a2534ac106b3737f64798d104d73bbc17a221c55
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-11-21 01:59:51 +0800

    /proc/pid/maps: dont allow paths with lineage to be listed
    (cherry picked from commit 5d62ef208a04b8a044af70a56ad43b1bac2d1b22)
    Change-Id: I96d4e56aab6cba15ec3d46cdb7e6a930922d5cf9
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -504,6 +504,11 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
 		dev = inode->i_sb->s_dev;
 		ino = inode->i_ino;
 		pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
+        struct dentry *dentry = file->f_path.dentry;
+        if (dentry) {
+        	const char *path = (const char *)dentry->d_name.name; 
+            	if (strstr(path, "lineage")) { return; }
+            	}
 #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
 		susfs_sus_kstat_spoof_show_map_vma(inode, &dev, &ino);
 #endif // #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
