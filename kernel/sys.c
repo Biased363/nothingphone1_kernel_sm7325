dea58d1044c3b4634d325ef058104c239a62b1f2
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-03-11 11:07:47 +0800

    syscall: Only spoof uname for root
    Change-Id: Iff2f407ca2e495f89c2c07e7c47af0020948ebd3

--- kernel/sys.c
+++ kernel/sys.c
@@ -1274,9 +1274,11 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4)) {
-		strcpy(tmp.release, "5.10.239");
-		pr_debug("fake uname: %s/%d release=%s\n",
-			 current->comm, current->pid, tmp.release);
+		if (current_uid().val == 0) {
+			strcpy(tmp.release, "5.10.239");
+			pr_debug("fake uname: %s/%d release=%s\n",
+				 current->comm, current->pid, tmp.release);
+		}
 	}
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 	if (static_branch_likely(&susfs_is_uname_spoof_buffer_set))
