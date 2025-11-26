d1823e205b7f2eb55d5d934a23c5b98d2a377d95
Ingo Molnar <mingo@kernel.org>
2025-11-26 05:31:28 +0100

    sched/fair: Separate se->vlag from se->vprot
    There's no real space concerns here and keeping these fields
    in a union makes reading (and tracing) the scheduler code harder.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://patch.msgid.link/20251201064647.1851919-4-mingo@kernel.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -468,15 +468,10 @@ struct sched_entity {
 	u32				child_burst_cnt;
 	u64				child_burst_last_cached;
 #endif
-	union {
-		/*
-		 * When !@on_rq this field is vlag.
-		 * When cfs_rq->curr == se (which implies @on_rq)
-		 * this field is vprot. See protect_slice().
-		 */
-		s64                     vlag;
-		u64                     vprot;
-	};
+	/* Approximated virtual lag: */
+	s64				vlag;
+	/* 'Protected' deadline, to give out minimum quantums: */
+	u64				vprot;
 	u64				slice;
 
 	u64				nr_migrations;
