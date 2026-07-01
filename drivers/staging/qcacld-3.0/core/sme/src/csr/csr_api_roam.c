af8523870294e693a13800d05e84d7b7fc3c5cbe
William24hmar <williamhmar24@gmail.com>
2026-07-01 17:30:29 +0000

    Merge tag 'LA.UM.9.14.6.r1-02800-QCM6490.QISI14.0' of https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0 into Staging-Kernel
    LA.UM.9.14.6.r1-02800-QCM6490.QISI14.0
    
    * tag 'clo/qcacld-3.0/LA.UM.9.14.6.r1-02800-QCM6490.QISI14.0':
      qcacld-3.0: Prevent ping loss due to runtime suspend
      qcacld-3.0: Add runtime pm lock during roaming
      qcacld-3.0: Add buffer overflow check in wma_fill_rx_stats function
      qcacld-3.0: Validate fse metadata before aggregation of FISA flow
      qcacld-3.0: Consider intersected AKM for association
      qcacld-3.0: Populate MBSSID cap in Ext CAP IE
      Release 2.0.8.35G
      qcacld-3.0: Fix underflow issue of beacon length
      Release 2.0.8.35F
      qcacld-3.0: don't overwrite psd_power flag if psd_set is true
      qcacld-3.0: Add TPE IE EIRP power support for 6 GHz band
      Release 2.0.8.35E
      qcacld-3.0: Validate bw in lim calculate tpc
      qcacld-3.0: Add Validation for WMA Handle and PSOC in Wake Event
      Release 2.0.8.35D
      qcacld-3.0: Add ini to apply RSSI delta for 6 GHz roam
      Release 2.0.8.35C
      qcacld-3.0: Update PMK from firmware for FT-SAE AKM also
      Release 2.0.8.35B
      qcacld-3.0: Rename variables name chan to chan_freq of wlan_hdd_mgmt_tx
      qcacld-3.0: Fix mgmt tx from supplicant failed on 6 GHz chan
      Release 2.0.8.35A
      qcacld-3.0: Fix potential OOB memory access
      Release 2.0.8.35
      qcacld-3.0: Recalculate TX power post CSA
      Release 2.0.8.34Z
      qcacld-3.0: Update key management in original auth mode for WAPI
      Release 2.0.8.34Y
      qcacld-3.0: Correcting the TSInfo structure size according to the Spec
      Release 2.0.8.34X
      qcacld-3.0: Remove use-after-free of frame in tx mgmt send
      Release 2.0.8.34W
      qcacld-3.0: Fix the possible OOB write in country IE unpack
      Release 2.0.8.34V
      qcacld-3.0: Enhance the RSNXE inter-op logic
      Release 2.0.8.34U
      qcacld-3.0: Set sar safety req resp event before unsolited work stop
      Release 2.0.8.34T
      qcacld-3.0: Update connect request crypto parameters
      qcacld-3.0: Enable CFG80211_MULTI_AKM_CONNECT_SUPPORT from kernelv6.0
      qcacld-3.0: Update wiphy max_num_akms_connect variable

--- drivers/staging/qcacld-3.0/core/sme/src/csr/csr_api_roam.c
+++ drivers/staging/qcacld-3.0/core/sme/src/csr/csr_api_roam.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2012-2020 The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -12611,20 +12611,16 @@ static QDF_STATUS csr_roam_start_wait_for_key_timer(
 {
 	QDF_STATUS status;
 	uint8_t session_id = mac->roam.WaitForKeyTimerInfo.vdev_id;
-#ifdef WLAN_DEBUG
 	tpCsrNeighborRoamControlInfo pNeighborRoamInfo =
 		&mac->roam.neighborRoamInfo[session_id];
-#endif
 
 	if (csr_neighbor_roam_is_handoff_in_progress(mac, session_id)) {
 		/* Disable heartbeat timer when hand-off is in progress */
-#ifdef WLAN_DEBUG
 		sme_debug("disabling HB timer in state: %s sub-state: %s",
 			mac_trace_get_neighbour_roam_state(
 				pNeighborRoamInfo->neighborRoamState),
 			mac_trace_getcsr_roam_sub_state(
 				mac->roam.curSubState[session_id]));
-#endif
 		mac->mlme_cfg->timeouts.heart_beat_threshold = 0;
 	}
 	sme_debug("csrScanStartWaitForKeyTimer");
@@ -17426,6 +17422,8 @@ static void csr_cm_update_driver_assoc_ies(
 		MIN_TX_PWR_CAP, MAX_TX_PWR_CAP};
 	uint8_t max_tx_pwr_cap = 0;
 	uint8_t supp_chan_ie[DOT11F_IE_SUPPCHANNELS_MAX_LEN], supp_chan_ie_len;
+	struct s_ext_cap *extcap;
+	uint8_t *ext_cap_ie;
 	static const uint8_t qcn_ie[] = {0x8C, 0xFD, 0xF0, 0x1,
 					 QCN_IE_VERSION_SUBATTR_ID,
 					 QCN_IE_VERSION_SUBATTR_DATA_LEN,
@@ -17436,6 +17434,14 @@ static void csr_cm_update_driver_assoc_ies(
 	rso_mode_cfg->assoc_ie_length = session->nAddIEAssocLength;
 	qdf_mem_copy(rso_mode_cfg->assoc_ie, session->pAddIEAssoc,
 		     rso_mode_cfg->assoc_ie_length);
+
+	ext_cap_ie = (uint8_t *)wlan_get_ie_ptr_from_eid(WLAN_ELEMID_XCAPS,
+							 rso_mode_cfg->assoc_ie,
+							 rso_mode_cfg->assoc_ie_length);
+	if (ext_cap_ie && wma_is_mbssid_enabled()) {
+		extcap = (struct s_ext_cap *)&ext_cap_ie[2];
+		extcap->multi_bssid = 1;
+	}
 
 	if (session->pConnectBssDesc)
 		max_tx_pwr_cap = csr_get_cfg_max_tx_power(
